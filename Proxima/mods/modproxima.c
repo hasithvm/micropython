3bbee5c201e6b3cd67f409a50543b11ccc9a19ff
Hasith V <hasith@vidanamadura.net>
2015-03-21 20:48:29 -0400

    Added capability for MicroPython to control Proxima

--- Proxima/mods/modproxima.c
+++ Proxima/mods/modproxima.c
@@ -9,12 +9,30 @@
 #include "mperror.h"
 #include "pyspi.h"
 #include "proxima.h"
+#include "devicectl.h"
 
 
+
+STATIC mp_obj_t proxima_mp_begin(void) {
+
+    assertControl();
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(proxima_mp_begin_obj, proxima_mp_begin);
+
+STATIC mp_obj_t proxima_mp_end(void) {
+    
+    deassertControl();
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(proxima_mp_end_obj, proxima_mp_end);
 
 STATIC const mp_map_elem_t mp_module_proxima_globals_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR_begin),                 (mp_obj_t)&proxima_mp_begin_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_end),                 (mp_obj_t)&proxima_mp_end_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR___name__),            MP_OBJ_NEW_QSTR(MP_QSTR_Proxima) },
-    // sub-objects
+    
+    // sub-objects specific to the Proxima platform.
     { MP_OBJ_NEW_QSTR(MP_QSTR_SPI),                (mp_obj_t)&proxima_spi_type },
 
 };
