1faa51540d1a757db5e8c36326b038b226b9c748
Robert Nelson <robert@rnelson.ca>
2015-03-26 21:30:27 -0400

    Upside down text works now

--- Proxima/pymod/http.py
+++ Proxima/pymod/http.py
@@ -1,20 +1,47 @@
 import socket
 import time
 
-def url_encode(msg, full=False):
-    msg = msg.replace("%", "%25")
 
-    for c in range(256):
-        if chr(c) == "%":
-            continue
-        if chr(c).lower() in "abcdefghijklmnopqrstuvwxyz":
-            continue
-        if chr(c) in "0123456789-_.~":
-            continue
-        if not full and (chr(c) in "!*'();:@&=+&,/?#[]"):
-            continue
-        msg = msg.replace(chr(c), "%{0}".format(hex(c)[2:].upper()))
-    return msg
+Xlate = {
+
+	'a':'ɐ', 'b':'q', 'c':'ɔ', 'd':'p', 'e':'ə',
+	'f':'ɟ', 'g':'ɓ', 'h':'ɥ', 'i':'\u0131', 'j':'ɾ',
+	'k':'ʞ', 'l':'l', 'm':'ɯ', 'n':'u', 'o':'o',
+	'p':'p', 'q':'q', 'r':'ɹ', 's':'s', 't':'ʇ',
+	'u':'n', 'v':'ʌ', 'w':'ʍ', 'x':'x', 'y':'ʎ',
+	'z':'z',
+
+	'A':'∀', 'B':'B', 'C':'Ↄ', 'D':'◖', 'E':'Ǝ',
+	'F':'Ⅎ', 'G':'⅁', 'H':'H', 'I':'I', 'J':'ſ',
+	'K':'K', 'L':'⅂', 'M':'W', 'N':'ᴎ', 'O':'O',
+	'P':'Ԁ', 'Q':'Ό', 'R':'ᴚ', 'S':'S', 'T':'⊥',
+	'U':'∩', 'V':'ᴧ', 'W':'M', 'X':'X', 'Y':'⅄',
+	'Z':'Z',
+
+	'0':'0', '1':'1', '2':'0', '3':'Ɛ', '4':'ᔭ',
+	'5':'5', '6':'9', '7':'Ɫ', '8':'8', '9':'0',
+
+	'_':'¯', "'":',', ',':"'", '\\':'/', '/':'\\',
+	'!':'¡', '?':'¿',
+}
+
+import sys
+
+def upsidedown(msg):
+	return ''.join([Xlate[c] if c in Xlate else c for c in reversed(msg)])
+
+
+def url_encode(msg, full=False):
+    msg = bytes(msg, "utf-8")
+    out = ""
+
+    for c in msg:
+        if chr(c).lower() in "abcdefghijklmnopqrstuvwxyz0123456789-_.~":
+            out += chr(c)
+        else:
+            out += "%{0}".format(hex(c)[2:].upper())
+
+    return out
 
 
 class http_request:
