766646f14759e4179f737d4479afdd450eaa092c
Robert Nelson <robert@rnelson.ca>
2015-04-12 00:20:03 -0400

    Added demo changes

--- Proxima/pymod/twitter.py
+++ Proxima/pymod/twitter.py
@@ -2,32 +2,47 @@ import http
 import crypto
 import ntp
 from ubinascii import b64encode
-#import time
+import time
 
-CONSUMER_KEY = "<consumer key>"
-CONSUMER_SECRET = "<consumer secret>"
+CONSUMER_KEY = "<key>"
+CONSUMER_SECRET = "<secret>"
 
-ACCESS_TOKEN = "<access token>"
-ACCESS_TOKEN_SECRET = "<access secret>"
+ACCESS_TOKEN = "<token>"
+ACCESS_TOKEN_SECRET = "<token_secret>"
+
+tweets = [ "Are you deliberately pushing my buttons?", "Proxima says hi from the SCE poster fair.", "My circuits are tingling...", "Why do people keep touching me?", "I felt that!", "Excuse me, you're squishing me.", "Please put me down...", "Can I help you?"]
 
 UPDATE_URL = "https://api.twitter.com/1.1/statuses/update.json"
 #MENTIONS_URL = 
 
+_tidx = 0
+
+def send_rnd(upside):
+    global _tidx
+    if upside:
+        print(update(http.upsidedown(tweets[_tidx])))
+    else:
+        print(update(tweets[_tidx]))
+
+    _tidx = (_tidx + 1) & 7
+
 def update(msg):
-    if len(msg) > 140:
+    if len(msg) > 130:
         raise ValueError("Tweets can only be 140 characters long")
 
-    time = str(ntp.gettimestamp())
+    timestamp = str(ntp.gettimestamp())
+    tu = time.localtime(int(timestamp) - 946684800 - 14400)
 
     url = UPDATE_URL
     req = http.http_request(url, "POST")
+    msg += " {0}:{1}:{2}".format(tu[3], tu[4], tu[5])
 
     hash_vars = {}
     hash_vars['status'] = http.url_encode(msg, True)
     hash_vars['oauth_consumer_key'] = CONSUMER_KEY
-    hash_vars['oauth_nonce'] = b64encode(crypto.sha1(time)).decode("utf-8").replace("+", "").replace("/","").replace("=","")
+    hash_vars['oauth_nonce'] = b64encode(crypto.sha1(timestamp)).decode("utf-8").replace("+", "").replace("/","").replace("=","")
     hash_vars['oauth_signature_method'] = "HMAC-SHA1"
-    hash_vars['oauth_timestamp'] = time #int(time.mktime(time.localtime()))
+    hash_vars['oauth_timestamp'] = timestamp #int(time.mktime(time.localtime()))
     hash_vars['oauth_token'] = ACCESS_TOKEN
     hash_vars['oauth_version'] = '1.0'
 
